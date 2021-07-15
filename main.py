3fab1e79ee1b47e2b2db831ccd1d5e4935ae78a0
Mohamed Mokhtar <rrrokhtar@gmail.com>
2021-07-15 15:12:04 +0200

    Cleaned main module

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-import torch
-import torch.nn
-import torch.optim
-import torch.utils.data
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-import torchvision.models as models
-import torch.backends.cudnn as cudnn
-import argparse
-import numpy as np
-import os
-import sys
-import time
-import datetime
-import shutil
-import copy
-import random
-import math
-import json
-import pickle
-import logging
-import logging.handlers
-import torch.utils.data.distributed
-import torch.nn.functional as F
 import yaml
 from common.util import Options
 from models.blocks import *
